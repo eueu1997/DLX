9883b6e0092daac6becef4a80dc7d3a4841b9fe0
eueu1997 <42282444+eueu1997@users.noreply.github.com>
2022-03-23 15:17:10 +0100

    a

--- datapath/phases/phase_3/phase3_comp/alu_comp/logic.vhd
+++ datapath/phases/phase_3/phase3_comp/alu_comp/logic.vhd
@@ -6,7 +6,7 @@ entity logic is
 	generic(nbit : integer);
 	port( a : in std_logic_vector(nbit-1 downto 0);
 		  b : in std_logic_vector(nbit-1 downto 0);
-		  sel : in std_logic_vector(3 downto 0);
+		  sel : in std_logic_vector(0 downto 3); 
 		  o : out std_logic_vector(nbit-1 downto 0));
 
 end logic;
